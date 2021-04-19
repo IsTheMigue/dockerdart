86d5d26f79469cd20c0c8818e6dce39a0e2e7e0e
IsTheMigue <82000518+IsTheMigue@users.noreply.github.com>
2021-04-19 02:40:02 -0500

    Update main.dart

--- app/main.dart
+++ app/main.dart
@@ -1,8 +1,25 @@
+//Paquete para convertir String en json
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+void main() {
+  
+  
+  HttpServer.bind("localhost", 8080).then((HttpServer server) {
+    server.listen((HttpRequest requestlisten) async {
+      Uri apiUrl = Uri.parse("https://my-json-server.typicode.com/IsTheMigue/Api/db");
+      HttpClientRequest request = await new HttpClient().getUrl(apiUrl);
+      HttpClientResponse response = await request.close();
 
-void main() async{
-  print("Hola Mundo");
+      Stream resStream = response.transform(Utf8Decoder());
 
+      await for (var data in resStream) {
+        requestlisten.response.write(data);
+      }
+      requestlisten.response.close();
+    });
+  });
+  
 }
-
 
 
